769c118ecac7ce8d2423e63e98e9a95a01b1372a
Konstantin S. <havendv@gmail.com>
2026-08-29 09:13:09 +0400

    feat: Updated OpenAPI spec (#45)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DoclingServe/Generated/DoclingServe.JsonSerializerContext.g.cs
+++ src/libs/DoclingServe/Generated/DoclingServe.JsonSerializerContext.g.cs
@@ -170,6 +170,7 @@ namespace DoclingServe
             typeof(global::DoclingServe.JsonConverters.UnixTimestampJsonConverter),
         })]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::DoclingServe.JsonSerializerContextTypes))]
+    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>), TypeInfoPropertyName = "SystemCollectionsGeneric_ObjectList")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::DoclingServe.ApiModelConfig))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(object))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::DoclingServe.BaseMeta))]
